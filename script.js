d5bff1447b686fd2ae1f02f769596ea89973a78e
Brian Barnes <barnesb7@gmail.com>
2017-04-28 20:28:01 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  $.get('https://www.reddit.com/r/turning.json').done(function(response){
+
+      console.log(response);
+
+      for (var i = 0; i < (response.data.children.length) - 2; i++){
+
+        if (response.data.children[i].data.thumbnail !== 'self'){
+
+              console.log(response.data.children[i].data.title);
+              var title = response.data.children[i].data.title;
+              var author = response.data.children[i].data.author;
+              var thumbNail = response.data.children[i].data.thumbnail;
+
+              var div = $('<div>').draggable();
+              $('.wrapper').append(div);
+              div.css('background-color', 'black');
+              div.css('border-radius', '5%');
+              div.css('border', "1px solid salmon")
+              div.attr('class', 'infoBox');
+
+              div.append('<p>'+ title +'</p>');
+              div.append('<img src = ' + thumbNail + '>');
+          }
+       }
+   });
+
+});
