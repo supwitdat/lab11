c82682d9ebfd1e6ccec7f76d6583c41da1681b76
Brian Barnes <barnesb7@gmail.com>
2017-04-28 22:35:27 -0400

    with jquery ui

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
       console.log(response);
 
-      for (var i = 0; i < (response.data.children.length) - 2; i++){
+      for (var i = 0; i < (response.data.children.length) - 1; i++){
 
         if (response.data.children[i].data.thumbnail !== 'self'){
 
@@ -17,13 +17,28 @@ $(document).ready(function(){
               $('.wrapper').append(div);
               div.css('background-color', 'black');
               div.css('border-radius', '5%');
-              div.css('border', "1px solid salmon")
+              div.css('border', "1px solid salmon");
               div.attr('class', 'infoBox');
+              div.attr('class', 'puff');
+
 
               div.append('<p>'+ title +'</p>');
               div.append('<img src = ' + thumbNail + '>');
           }
        }
    });
+
+   $('button' ).click(function() {
+     $( ".puff" ).toggle( "puff" );
+
+      console.log($(this).text());
+
+     if ($(this).text() === 'DESTROY!!'){
+        $(this).text('RESURRECT!!');
+     } else if ($(this).text() === 'RESURRECT!!'){
+        $(this).text('DESTROY!!');
+     }
+   });
+
 
 });
